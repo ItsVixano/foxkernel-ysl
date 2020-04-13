228d3babbd29e472ea9db1ce7890f7a08cf73bc8
itsvixano <vixanonew@gmail.com>
2020-04-13 20:31:16 +0000

    uodatdated build.sh for 9.3.0

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
-git clone https://github.com/RevanthTolety/Toolchain ~/Toolchain
+rm -rf out
+rm -rf zip
+git clone https://github.com/arter97/arm64-gcc ~/toolchain
 export ARCH=arm64
-export CROSS_COMPILE=~/Toolchain/bin/aarch64-linux-android-
-mkdir output
-make -C $(pwd) O=output ysl_defconfig
-make -j32 -C $(pwd) O=output
-git clone https://github.com/mahajant99/AnyKernel3.git -b ysl-aosp zip
-cp -r output/arch/arm64/boot/Image.gz-dtb zip/
+export CROSS_COMPILE=~/toolchain/bin/aarch64-elf-
+mkdir out
+make O=out clean
+make O=out mrproper
+make -C $(pwd) O=out Fox_ysl_defconfig
+make -j32 -C $(pwd) O=out
+git clone https://github.com/ItsVixano/AnyKernel3 -b ysl-aosp zip
+cp -r out/arch/arm64/boot/Image.gz-dtb zip/
 cd zip
-mv Image.gz-dtb zImage 
-zip -r Test-Pie-kernel.zip *
+mv Image.gz-dtb zImage
+zip -r Fox_kernel_V4-oc.zip *
